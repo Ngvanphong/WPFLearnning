9b8232d9332481218f1f610dea8fddd18cf93e2f
NGUYENVANPHONG <ngvanphong2012@gmail.com>
2020-04-18 22:28:52 +0900

    test 2

---
+++ WpfLearn/WpfLearn/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLearn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void TextBlock_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    TextBlock textBlock = sender as TextBlock;
+        //    textBlock.Text = "Change Text Block 1";
+        //}
+
+        //private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        //{
+        //    Process.Start(e.Uri.ToString());
+        //    TextBlock ob = sender as TextBlock;
+        //    textBlock2.Text = textblock1.Inlines.ElementAt(0).ToString();
+        //    textBlock2.Inlines.Add(new Run("Text new Add") { FontSize = 30 });
+        //}
+
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
